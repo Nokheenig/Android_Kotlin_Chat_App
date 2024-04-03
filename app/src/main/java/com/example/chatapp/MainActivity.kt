cf931b17857a080af175883ad023433323e1fc86
Yoann <yoann.chamillard@gmail.com>
2024-04-03 16:43:26 +0200

    234-235.Ask for permission for the internal storage and get the Image

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.chatapp
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.example.chatapp.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
+    private lateinit var getResult: ActivityResultLauncher<Intent>
+    private val STORAGE_REQUEST_CODE = 1293487
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
@@ -29,6 +38,20 @@ class MainActivity : AppCompatActivity() {
         }
         mBinding.textViewSignUp.setOnClickListener {
             startPreviousAnimation()
+        }
+        mBinding.profileImage.setOnClickListener{
+            if (ActivityCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+                requestPermission()
+            } else {
+                getImage()
+            }
+        }
+
+        getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if (it.resultCode == RESULT_OK) {
+                mBinding.profileImage.setImageURI(it.data?.data)
+            }
         }
     }
 
@@ -84,5 +107,52 @@ class MainActivity : AppCompatActivity() {
         mBinding.flipper.setInAnimation(this,android.R.anim.slide_in_left)
         mBinding.flipper.setOutAnimation(this,android.R.anim.slide_out_right)
         mBinding.flipper.showPrevious()
+    }
+
+    private fun getImage(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        getResult.launch(intent)
+    }
+
+    private  fun requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+            // This function checks if we should show the user why we need this permission,
+            // and this happens if the user denies the permissions before and tries to access again a functionnality requiring it
+            //Log.d("dBug", "test if shouldShowRequestPermissionRationale case")
+            AlertDialog.Builder(this@MainActivity)
+                .setPositiveButton(R.string.dialog_button_yes) { _,_ ->
+                    ActivityCompat.requestPermissions(this@MainActivity,
+                        arrayOf(
+                            android.Manifest.permission.READ_EXTERNAL_STORAGE
+                        ),
+                        STORAGE_REQUEST_CODE)
+                }.setNegativeButton(R.string.dialog_button_no){
+                    dialog, _ ->
+                    dialog.cancel()
+                }.setTitle("Permission needed")
+                .setMessage("This permission is needed for accessing the internal storage.")
+                .show()
+        } else {
+            ActivityCompat.requestPermissions(this@MainActivity,
+                arrayOf(
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ),
+                STORAGE_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == STORAGE_REQUEST_CODE && grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            getImage()
+        } else {
+            Toast.makeText(this@MainActivity, "Permission not granted", Toast.LENGTH_LONG).show()
+        }
     }
 }
